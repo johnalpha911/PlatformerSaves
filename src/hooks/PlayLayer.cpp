c34352590d722c86327c30576b4741e9e1d43d86
Johnalpha <johnalpha911@gmail.com>
2026-08-14 11:21:35 +0300

    Add checkpoint gate diagnostic

--- src/hooks/PlayLayer.cpp
+++ src/hooks/PlayLayer.cpp
@@ -170,6 +170,7 @@ void PSPlayLayer::postUpdate(float i_unkFloat) {
 CheckpointObject* PSPlayLayer::markCheckpoint() {
     PSCheckpointObject* l_checkpointObject = static_cast<PSCheckpointObject*>(PlayLayer::markCheckpoint());
 
+    log::info("PSDBG cp obj={} en={} post={} prac={} tried={} act={}", l_checkpointObject != nullptr, savesEnabled(), m_fields->m_inPostUpdate, m_isPracticeMode, m_fields->m_triedPlacingCheckpoint, m_activatedCheckpoint != nullptr);
     if (l_checkpointObject && savesEnabled() && m_fields->m_inPostUpdate && !m_isPracticeMode) {
         if (m_fields->m_triedPlacingCheckpoint) {
             m_fields->m_triedPlacingCheckpoint = false;
